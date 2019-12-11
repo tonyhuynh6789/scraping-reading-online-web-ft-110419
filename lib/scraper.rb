38bca5043ccead1130c6a97f7e34ad3504e0bd47
Tony Rim <duc_thinh_03@yahoo.com>
2019-12-11 22:49:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,3 +15,6 @@ require 'open-uri'
  
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN")
+
+
+p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0]
